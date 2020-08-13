a2e851022248f057691f4aef2ff6312386f8fac7
unknown <martooo1936@gmail.com>
2020-08-13 23:33:26 +0200

    adding email attribute to registerUserRequest

--- TheRetinoblastomaWiki.Server/TheRetinoblastomaWiki.Server/Models/Identity/RegisterUserRequestModel.cs
+++ TheRetinoblastomaWiki.Server/TheRetinoblastomaWiki.Server/Models/Identity/RegisterUserRequestModel.cs
@@ -8,6 +8,9 @@ namespace TheRetinoblastomaWiki.Server.Models.Identity
         public string Username { get; set; }
 
         [Required]
+        public string Email { get; set; }
+
+        [Required]
         public string Password { get; set; }
     }
 }

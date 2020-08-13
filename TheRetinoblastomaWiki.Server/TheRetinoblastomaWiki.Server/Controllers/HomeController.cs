12dc4fedc2c6f835cc63d8ef480250d79fd67586
unknown <martooo1936@gmail.com>
2020-08-13 21:45:12 +0200

    structure clean up

--- TheRetinoblastomaWiki.Server/TheRetinoblastomaWiki.Server/Controllers/HomeController.cs
+++ TheRetinoblastomaWiki.Server/TheRetinoblastomaWiki.Server/Controllers/HomeController.cs
@@ -1,37 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using TheRetinoblastomaWiki.Server.Models;
-
+﻿
 namespace TheRetinoblastomaWiki.Server.Controllers
 {
+    using System.Diagnostics;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Logging;
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+   
     }
 }

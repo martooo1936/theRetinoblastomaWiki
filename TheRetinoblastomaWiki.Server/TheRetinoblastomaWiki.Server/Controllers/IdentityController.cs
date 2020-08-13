0f76c24ea34dbff8b0ffb15497e3a4552e9beaf7
unknown <martooo1936@gmail.com>
2020-08-14 00:30:17 +0200

    implementation of login functionality

--- TheRetinoblastomaWiki.Server/TheRetinoblastomaWiki.Server/Controllers/IdentityController.cs
+++ TheRetinoblastomaWiki.Server/TheRetinoblastomaWiki.Server/Controllers/IdentityController.cs
@@ -6,14 +6,27 @@ namespace TheRetinoblastomaWiki.Server.Controllers
     using System.Threading.Tasks;
     using TheRetinoblastomaWiki.Server.Models.Identity;
     using Data.Models;
+    using System.Security.Claims;
+    using Microsoft.IdentityModel.Tokens;
+    using Microsoft.Extensions.Options;
+    using System.IdentityModel.Tokens.Jwt;
+    using System;
+    using System.Text;
 
     public abstract class IdentityController : ApiController
     {
 
         private readonly UserManager<User> userManager;
+        private readonly AppSettings appSettings;
 
+        protected IdentityController(UserManager<User> userManager,
+            IOptions<AppSettings> appSettings)
+        { 
+        this.userManager = userManager;
+        this.appSettings = appSettings.Value;
+        }
         // creating the register functionality
-        public async Task<IActionResult> Register(RegisterUserRequestModel model)
+        public async Task<IActionResult> Register(RegisterRequestModel model)
         {
             var user = new User
             {
@@ -34,6 +47,40 @@ namespace TheRetinoblastomaWiki.Server.Controllers
 
         // creating the login functionality
 
-        public async Task<IActionResult> Login()
+        public async Task<ActionResult<string>> Login(LoginRequestModel model) 
+        {
+            // validation if user is logged in successfully
+
+            var user = await this.userManager.FindByNameAsync(model.UserName);
+            if (user == null)
+            {
+                return Unauthorized();
+            }
+
+            var passwordValid = await this.userManager.CheckPasswordAsync(user, model.Password);
+
+            if (!passwordValid)
+            {
+                return Unauthorized();
+            }
+            //the logic behind creating the JWT Token
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(this.appSettings.Secret);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, user.Id.ToString())
+                }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var encryptedToken = tokenHandler.WriteToken(token);
+
+            return encryptedToken;
+        }
     }
 }
